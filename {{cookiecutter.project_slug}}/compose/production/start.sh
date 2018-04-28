7a655c057e0b29ce7383309f6f8942bcc2813e78
Timmy O'Mahony <hey@timmyomahony.com>
2018-04-28 11:15:54 +0100

    Create start.sh

---
+++ {{cookiecutter.project_slug}}/compose/production/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/admin/{{cookiecutter.project_slug}}/
+docker-compose -f production.yml build
+docker-compose -f production.yml run django python manage.py migrate
+docker-compose -f production.yml run django python manage.py collectstatic --noinput
+
+docker-compose -f production.yml down
+docker-compose -f production.yml up
